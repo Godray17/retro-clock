802a6c00baa6e279d8d93cec575d35227a9239b6
Godray17 <54802896+Godray17@users.noreply.github.com>
2020-08-24 21:59:06 +1000

    добавил нули при получении однозначных значений.

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 function timer() {
   var date = new Date
-  var hours = date.getHours()
+var hours = date.getHours()
+// var hours = 1
   var minutes = date.getMinutes()
+// var minutes = 1
+
+if (hours < 10) {
+  hours = "0" + hours;
+}
+
+if (minutes < 10) {
+  minutes = "0" + minutes;
+}
 
   document.getElementById("hr").textContent = hours;
   document.getElementById("min").textContent = minutes;
